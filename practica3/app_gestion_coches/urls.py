b0c45f0ecbd34a58b601b6987bef4b058ce9f616
Javier Pozo González <jpozogo.inf@upsa.es>
2026-03-05 13:30:30 +0100

    Implementacion de la practica 4 completa, con peticiones extra como busqueda por Modelo de coches, coche con sus servicios, ...

--- practica3/app_gestion_coches/urls.py
+++ practica3/app_gestion_coches/urls.py
@@ -1,7 +1,22 @@
 from django.urls import path
-from .views import lista_clientes, detalle_cliente
+from .views import (
+    lista_clientes, registrar_cliente, registrar_coche, registrar_servicio,
+    buscar_cliente_por_email, buscar_coches_por_marca, listar_servicios_taller,
+    buscar_cliente, buscar_coche_por_matricula, buscar_coches_de_cliente, buscar_servicios_de_coche
+)
 
 urlpatterns = [
     path('clientes/', lista_clientes, name='lista_clientes'),
-    path('clientes/<int:cliente_id>/', detalle_cliente, name='detalle_cliente'),
+    path('clientes/registrar/', registrar_cliente, name='registrar_cliente'),
+    path('coches/registrar/', registrar_coche, name='registrar_coche'),
+    path('servicios/registrar/', registrar_servicio, name='registrar_servicio'),
+    
+    path('clientes/email/<str:email>/', buscar_cliente_por_email, name='buscar_cliente_por_email'),
+    path('coches/marca/<str:marca>/', buscar_coches_por_marca, name='buscar_coches_por_marca'),
+    path('servicios/todos/', listar_servicios_taller, name='listar_servicios_taller'),
+    
+    path('clientes/<int:cliente_id>/', buscar_cliente, name='buscar_cliente'),
+    path('coches/matricula/<str:matricula>/', buscar_coche_por_matricula, name='buscar_coche_por_matricula'),
+    path('clientes/<int:cliente_id>/coches/', buscar_coches_de_cliente, name='buscar_coches_de_cliente'),
+    path('coches/<int:coche_id>/servicios/', buscar_servicios_de_coche, name='buscar_servicios_de_coche'),
 ]
